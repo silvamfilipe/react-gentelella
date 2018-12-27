c5e2142d922beaab026b783cbed7d6dcfc0b85a5
Filipe Silva <silvam.filipe@gmail.com>
2018-12-27 00:40:54 -0100

    Added some more code samples

--- example/src/scenes/extras/plain-page.js
+++ example/src/scenes/extras/plain-page.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Page, PageTitle, PanelBody, Panel, PanelHeader } from 'react-gentelella';
 import GlobalSearch from "../app/global-search";
 import GeneralPanelToolbox from "../app/general-panel-toolbox";
+import CodeSample from '../app/code-sample'
+import Highlight from 'react-highlight'
 
-const PlainPage = () => (
-  <Page>
-    <PageTitle title={"Plain Page"}>
-        <GlobalSearch />
-    </PageTitle>
-      <Panel>
+class PlainPage extends Component {
+
+  state = {
+    showCode: false
+  };
+
+  renderCodeSample = () => {
+    const { showCode } = this.state;
+    return (
+      <CodeSample open={showCode}>
+        <Highlight language="javascript">
+          {
+          "import React from 'react';\n" +
+          "import { Page, PageTitle, PanelBody, Panel, PanelHeader } from 'react-gentelella';\n\n" +
+          "const PageExample = () => (\n  <Page>\n" +
+          "    <PageTitle title={\"Plain Page\"}/>\n" +
+          "    <Panel>\n" +
+          "      <PanelHeader>\n" +
+          "        <h2>Plain Page</h2>\n" +
+          "      </PanelHeader>\n" +
+          "      <PanelBody>\n" +
+          "        Add content to the page ...\n" +
+          "      </PanelBody>\n" +
+          "    </Panel>\n" +
+          "  </Page>\n);\n\n" +
+          "export default PageExample;\n"
+          }
+        </Highlight>
+      </CodeSample>
+    );
+  };
+
+  render() {
+    return (
+      <Page>
+        <PageTitle title={"Plain Page"}>
+          <GlobalSearch />
+        </PageTitle>
+        <Panel>
           <PanelHeader>
             <h2>Plain Page</h2>
-              <GeneralPanelToolbox/>
+            <GeneralPanelToolbox onCodeClick={ () => this.setState({showCode: !this.state.showCode})}/>
           </PanelHeader>
           <PanelBody>
-              Add content to the page ...
+            { this.renderCodeSample() }
+            Add content to the page ...
           </PanelBody>
-      </Panel>
-  </Page>
-);
+        </Panel>
+      </Page>
+    );
+  }
+}
 
 export default PlainPage;
--- example/src/scenes/ui-elements/general-elements/right-tabs.js
+++ example/src/scenes/ui-elements/general-elements/right-tabs.js
@@ -1,30 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Tabs, Tab, Panel, PanelHeader, PanelBody } from 'react-gentelella';
 import GeneralPanelToolbox from '../../app/general-panel-toolbox'
+import CodeSample from '../../app/code-sample'
+import Highlight from 'react-highlight'
 
-const RightTabs = () => (
-  <Panel>
-    <PanelHeader>
-      <h2>
-        <i className={'fa fa-bars'}/>
-        &nbsp;Tabs<small>Float right</small>
-      </h2>
-      <GeneralPanelToolbox/>
-    </PanelHeader>
-    <PanelBody>
-      <Tabs right>
-        <Tab title={"Home"} active>
-          Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terr.
-        </Tab>
-        <Tab title={"Profile"}>
-          Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip
-        </Tab>
-        <Tab title={"Profile"}>
-          xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk
-        </Tab>
-      </Tabs>
-    </PanelBody>
-  </Panel>
-);
+class RightTabs extends Component {
+
+  state = {
+    showCode: false,
+  };
+
+  renderCodeSample = () => {
+    const { showCode } = this.state;
+    return (
+      <CodeSample open={showCode}>
+        <Highlight language="javascript">
+          {
+            "import React from 'react';\n" +
+            "import { Tabs, Tab } from 'react-gentelella';\n\n" +
+            "const TabsExample = () => (\n  <Tabs right>\n" +
+            "    <Tab title={\"Home\"} active>\n" +
+            "      Raw denim you probably haven't heard of them jean shorts Austin...\n" +
+            "    </Tab>\n" +
+            "    <Tab title={\"Profile\"}>\n" +
+            "      Food truck fixie locavore, accusamus mcsweeney's marfa nulla...\n" +
+            "    </Tab>\n" +
+            "    <Tab title={\"Profile\"}>\n" +
+            "      xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee...\n" +
+            "    </Tab>\n" +
+            "  </Tabs>\n);\n\n" +
+            "export default TabsExample;"
+          }
+        </Highlight>
+      </CodeSample>
+    );
+  };
+
+  render() {
+    return (
+      <Panel>
+        <PanelHeader>
+          <h2>
+            <i className={'fa fa-bars'}/>
+            &nbsp;Tabs<small>Float right</small>
+          </h2>
+          <GeneralPanelToolbox onCodeClick={ () => this.setState({showCode: !this.state.showCode})}/>
+        </PanelHeader>
+        <PanelBody>
+          { this.renderCodeSample() }
+          <Tabs right>
+            <Tab title={"Home"} active>
+              Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terr.
+            </Tab>
+            <Tab title={"Profile"}>
+              Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip
+            </Tab>
+            <Tab title={"Messages"}>
+              xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk
+            </Tab>
+          </Tabs>
+        </PanelBody>
+      </Panel>
+    );
+  }
+}
 
 export default RightTabs;
