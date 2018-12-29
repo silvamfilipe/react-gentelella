7761acede89d604bf02667c10c070435e791c084
Filipe Silva <silvam.filipe@gmail.com>
2018-12-29 00:32:51 -0100

    Added drop downs

--- example/src/scenes/ui-elements/general-elements.js
+++ example/src/scenes/ui-elements/general-elements.js
@@ -4,13 +4,14 @@ import GlobalSearch from "../app/global-search";
 import LeftTabs from './general-elements/left-tabs'
 import RightTabs from './general-elements/right-tabs'
 import VerticalTabsLeft from "./general-elements/vertical-tabs-left";
-import VerticalTabsRight from './general-elements/vertical-tabs-right'
-import AccordionBox from './general-elements/accordion-box'
-import Popovers from './general-elements/popovers'
-import Tooltips from './general-elements/tooltips'
-import Jumbotron from './general-elements/jumbotron'
-import Modals from './general-elements/modals'
-import { Clearfix } from 'react-bootstrap'
+import VerticalTabsRight from './general-elements/vertical-tabs-right';
+import AccordionBox from './general-elements/accordion-box';
+import Popovers from './general-elements/popovers';
+import Tooltips from './general-elements/tooltips';
+import Jumbotron from './general-elements/jumbotron';
+import Modals from './general-elements/modals';
+import Dropdowns from './general-elements/dropdowns';
+import { Clearfix } from 'react-bootstrap';
 
 const GeneralElements = () => (
   <Page>
@@ -51,6 +52,7 @@ const GeneralElements = () => (
       </div>
       <div className="col-md-6 col-sm-6 col-xs-12">
           <Modals/>
+          <Dropdowns/>
       </div>
       <div className="col-md-6 col-sm-6 col-xs-12">
         <Jumbotron/>
