376da7796da76bfa7302f9c727fba21c30216e6b
Filipe Silva <silvam.filipe@gmail.com>
2019-01-14 17:17:09 -0100

    Updates

---
+++ precache-manifest.6d01ea7ad5918212e355a0eb5f784c99.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "083a3bc4905e63653d1e",
+    "url": "/react-gentelella/static/css/main.a7e267e6.chunk.css"
+  },
+  {
+    "revision": "083a3bc4905e63653d1e",
+    "url": "/react-gentelella/static/js/main.083a3bc4.chunk.js"
+  },
+  {
+    "revision": "db70304859fe0c5669f0",
+    "url": "/react-gentelella/static/css/1.b147e1cf.chunk.css"
+  },
+  {
+    "revision": "db70304859fe0c5669f0",
+    "url": "/react-gentelella/static/js/1.db703048.chunk.js"
+  },
+  {
+    "revision": "a4d3b8bc335a49f8f76c",
+    "url": "/react-gentelella/static/js/runtime~main.a4d3b8bc.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "26d1a62908b029a9723851295998d4d6",
+    "url": "/react-gentelella/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f606557bf0d8e56153f85419400763f0.js"
+  "/react-gentelella/precache-manifest.6d01ea7ad5918212e355a0eb5f784c99.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-gentelella/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
