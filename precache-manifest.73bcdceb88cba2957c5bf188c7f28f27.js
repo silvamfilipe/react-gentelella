e2deda7504f7234cc08d793b84187d6d0f08bcc5
Filipe Silva <silvam.filipe@gmail.com>
2019-04-26 17:24:32 +0000

    Updates

---
+++ precache-manifest.73bcdceb88cba2957c5bf188c7f28f27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fe4068b3385b9153c852eb19ea3423",
+    "url": "/react-gentelella/index.html"
+  },
+  {
+    "revision": "c58d82b57f1d37108e6d",
+    "url": "/react-gentelella/static/css/2.f584b549.chunk.css"
+  },
+  {
+    "revision": "9a4f3c78a86b3da73591",
+    "url": "/react-gentelella/static/css/main.e87b7fdf.chunk.css"
+  },
+  {
+    "revision": "c58d82b57f1d37108e6d",
+    "url": "/react-gentelella/static/js/2.60197a66.chunk.js"
+  },
+  {
+    "revision": "9a4f3c78a86b3da73591",
+    "url": "/react-gentelella/static/js/main.74712313.chunk.js"
+  },
+  {
+    "revision": "a79939d78f5c0e298a2d",
+    "url": "/react-gentelella/static/js/runtime~main.8123ea69.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-gentelella/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
